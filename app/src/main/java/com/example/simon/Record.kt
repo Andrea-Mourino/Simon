845c94a23daf86ad4384fd344cb5f2911973d7f8
amourinodesousa@danielcastelao.org <tu_correo@example.com>
2025-12-04 19:38:55 +0100

    agregando Record

---
+++ app/src/main/java/com/example/simon/Record.kt
@@ -0,0 +1,8 @@
+package com.example.simon
+import java.time.LocalDate
+
+//Esta clase sirve para almacenar, pasar y mostrar de manera organizada los datos del récord
+
+//Almacena la ronda mas alta alcanzada y almacena la fecha
+
+data class Record(var record: Int, var date: LocalDate)
