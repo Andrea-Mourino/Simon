2781d64a81a0b97e90443686e14e9ef536a1a3da
amourinodesousa@danielcastelao.org <tu_correo@example.com>
2025-12-04 19:37:18 +0100

    agregando InterfazConexion

--- app/src/main/java/com/example/simon/InterfazConexion.kt
+++ app/src/main/java/com/example/simon/InterfazConexion.kt
@@ -2,7 +2,9 @@ package com.example.simon
 
 import android.content.Context
 
+//interface sirve para desacoplar la capa de datos del ViewModel en caso de que cambiara el SharePreferences pues el ViewModel no se veria afectado
 interface InterfazConexion {
-    fun obtenerRecord(context: Context):Record
-    fun actuaizarRecord(record:Int,context: Context):Record
+    fun obtenerRecord(context: Context):Record //Esto declara una función que debe devolver un Record dado un Context
+    fun actualizarRecord(record:Int,context: Context):Record //Esto declara una función que debe actualizar el record y devolver el record actualizado
+    //También recibe Context
 }
