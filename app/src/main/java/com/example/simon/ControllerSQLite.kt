b7a295b529c86de992a405e92df2d25f73729818
amourinodesousa@danielcastelao.org <tu_correo@example.com>
2025-12-11 12:38:55 +0100

    ControllerSQLite

---
+++ app/src/main/java/com/example/simon/ControllerSQLite.kt
@@ -0,0 +1,19 @@
+package com.example.simon
+
+import android.content.Context
+
+object ControllerSQLite: InterfazConexion {
+    override fun obtenerRecord(context: Context): Record {
+        TODO("Not yet implemented")
+    }
+
+    override fun actualizarRecord(
+        record: Int,
+        context: Context
+    ): Record {
+        TODO("Not yet implemented")
+    }
+
+
+
+}
