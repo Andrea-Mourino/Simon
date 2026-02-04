2959b79330b1405c67694c22216c7d54e337e3e2
AlexandreSinisterra <agarciasinisterra@danielcastelao.org>
2026-02-04 01:35:35 +0100

    pequeño comentario

--- app/src/main/java/com/example/simon/room/AppDatabase.kt
+++ app/src/main/java/com/example/simon/room/AppDatabase.kt
@@ -8,6 +8,7 @@ import androidx.room.RoomDatabase
  *         Entity::class,
  *         Entity2::class
  *     ],
+ *
  */
 @Database(entities = [Entity::class], version = 1) // definismos la configuracion junto a sus entidades
 abstract class AppDatabase : RoomDatabase() { // Clase abstracta que expone el DAO para las Entidades de la ddbb
